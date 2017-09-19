8722c4eaafc7bc3ea1f53d87943bce9acb286a62
Marijn Haverbeke <marijn@haverbeke.nl>
2017-09-19 11:40:50 +0200

    Make rollup config agree with package.json on file name

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   input: "./src/find-edits.js",
-  output: {format: "cjs", file: "dist/index.js"},
+  output: {format: "cjs", file: "dist/find-edits.js"},
   sourcemap: true,
   plugins: [require("rollup-plugin-buble")()],
   external(id) { return !/^[\.\/]/.test(id) }
