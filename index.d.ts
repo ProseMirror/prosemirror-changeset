74f573c0fe1b47e9a9fcba6510a074ea844539d9
Matias Piipari <matias.piipari@gmail.com>
2018-08-08 13:43:16 +0100

    Adds TypeScript type declarations.

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+import { Slice } from "prosemirror-model"
+import { StepMap } from "prosemirror-transform"
+
+export class Span {
+  readonly from: number
+  readonly to: number
+  readonly data: any
+}
+
+export class DeletedSpan extends Span {
+  readonly pos: number
+  readonly slice: Slice
+}
+
+export type Metadata = any[] | {[key: string]: any}
+
+export class Changeset {
+  readonly inserted: Span[]
+  readonly deleted: DeletedSpan[]
+  addSteps (newDoc: Node, maps: ReadonlyArray<StepMap>, data: Metadata): Changeset
+  static create (doc: Node, object ?: { compare: (a: Metadata, b: Metadata) => boolean, combine: (a: Metadata) => Metadata})
+}
